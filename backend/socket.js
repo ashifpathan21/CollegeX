bfc2f22d6d8a3ffb2a557c2e7447ad294f3004fc
Ashif Pathan <ashifbgmiking@gmail.com>
2025-07-01 16:19:34 +0530

    Update socket.js

--- backend/socket.js
+++ backend/socket.js
@@ -7,7 +7,7 @@ let io;
 export function initializeSocket(server) {
   io = new Server(server, {
     cors: {
-      origin: ['http://localhost:5173'],
+      origin: ['http://localhost:5173' , 'https://collegex-qzm7.onrender.com'],
       methods: ['GET', 'POST'],
       credentials: true,
     },
